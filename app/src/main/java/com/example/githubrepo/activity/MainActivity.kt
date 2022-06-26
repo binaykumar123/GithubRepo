98041da4005bf16430430794f9da8f2eee8c6397
Janani <janani@jify.co>
2022-06-26 17:51:06 +0530

    initial project structure

---
+++ app/src/main/java/com/example/githubrepo/activity/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.githubrepo.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.githubrepo.R
+import com.example.githubrepo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
